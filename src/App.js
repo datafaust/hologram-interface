8c5bf4f03cd3f05cb394f11801feb43c8a94a70e
mac <mac@macs-MacBook-Pro.local>
2021-02-14 23:41:19 -0500

    update interface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import classes from './app.module.css'
+import sun from './assets/pics/sol.png'
+import genre from './assets/pics/girlPointing.png'
+import song from './assets/pics/guyListening.png'
+import search from './assets/pics/nuclearCowboy.png'
+import Processing from './Processing';
 
-function App() {
+
+const api = "http://192.168.0.20:5000"
+
+const App = () => {
+
+  const [request, setRequest] = useState([]);
+  const [songColor, setsongColor] = useState("#b29700");
+  const [genreColor, setgenreColor] = useState("#b29700");
+  const [loading, setLoading] = useState(false)
+
+
+  const getSong = async () => {
+    console.log('getting song stuff...')
+    setsongColor("#aec6cf");
+    //setLoading(true)
+
+    let response = await fetch(`${api}/recognize`)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res["videos"][0])
+        return res["videos"][0]["url_suffix"]
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+
+    if(response) {
+      console.log(response)
+      setsongColor("#b29700")
+      //setLoading(false)
+    } 
+  }
+
+  const getGenre = async () => {
+    console.log('getting song stuff...')
+    setgenreColor("#aec6cf");
+
+    let response = await fetch(`${api}/recognize`)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res["videos"][0])
+        return res["videos"][0]["url_suffix"]
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+
+    if(response) {
+      console.log(response)
+      setgenreColor("#b29700")
+    } 
+  } 
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+    className={classes.container}
+      //id="animate-area"
+    >
+      {
+        !loading ?
+        <div>
+
+        {/** BRAND */}
+        <div className={classes.brandContainer}>
+          <div className={classes.brandMain}>
+            <img src={sun} height={80} width={80} />
+            <p>FAUSTIAN INDUSTRIES</p>
+          </div>
+          <p className={classes.brandSub}>For A Brighter Tomorrow</p>
+        </div>
+
+
+
+        <br />
+        {/** OPTIONS */}
+        {/* <div className={classes.title}>Choose your mode below</div> */}
+        <div className={classes.choiceContainer}>
+          <div onClick={()=>getSong()} className={classes.choice} style={{backgroundColor: songColor}}>
+            <img className={classes.choiceImg} style={{marginTop:'5%'}} src={song} height={180} width={180} />
+            <p className={classes.choiceText}>RECOGNIZE SONG</p>
+          </div>
+          <div onClick={()=>getGenre()} className={classes.choice} style={{backgroundColor: genreColor }}>
+          <img className={classes.choiceImg} src={genre} height={190} width={150} />
+            <p className={classes.choiceText}>RECOGNIZE GENRE</p>
+          </div>
+          <div onClick={()=>getGenre()} className={classes.choice} style={{backgroundColor: genreColor }}>
+          <img className={classes.choiceImg} src={search} height={190} width={270} style={{ marginLeft: "-2%"}} />
+            <p className={classes.choiceText}>EXPLORE</p>
+          </div>
+        </div>
+
+        } </div> : 
+        <Processing 
+        /> 
+      }
+      
     </div>
   );
-}
+};
 
 export default App;
